17d194dd1d9b0298d1bab1d25504770e1149d02a
James Barbour <jameskbarbour1@gmail.com>
2025-06-17 17:11:07 +0000

    Initial commit

---
+++ src/tsoa/hello.route.ts
@@ -0,0 +1,9 @@
+import { Get, Route, Path } from 'tsoa'
+
+@Route('hello')
+export class HelloRoute {
+  @Get('{name}')
+  public async sayHello (@Path() name: string): Promise<string> {
+    return `Hello ${name}`
+  }
+}
