5ebaec821221ed8db99b47bb50090b9011b85d27
James Barbour <jameskbarbour1@gmail.com>
2025-06-23 13:51:34 +0000

    Update for OpenAPI generation

---
+++ generate-openapi.sh
@@ -0,0 +1,3 @@
+npx tsoa spec-and-routes
+
+npx @openapitools/openapi-generator-cli generate -i ./src/docs/swagger.json -o ./client -g typescript-fetch
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vitest/config'
+import openApiPlugin from './vitest-openapi-plugin'
 
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
-    setupFiles: ['./database_test_setup.ts']
-  }
+    setupFiles: ['./database_test_setup.ts'],
+    watchExclude: ['src/routes/generated/**', 'client/**']
+  },
+  plugins: [openApiPlugin]
 })
