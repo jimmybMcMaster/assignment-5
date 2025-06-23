ce69464c73d0a2969520a620f325a198ddd0817e
James Barbour <jameskbarbour1@gmail.com>
2025-06-23 00:40:04 +0000

    Add tsoa/swagger ui

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import Koa from 'koa'
 import cors from '@koa/cors'
 import zodRouter from 'koa-zod-router'
 import qs from 'koa-qs'
+import Router from '@koa/router'
+import { koaSwagger } from 'koa2-swagger-ui'
+import { RegisterRoutes } from './src/routes/generated/routes'
+import * as swagger from './src/docs/swagger.json'
+import bodyParser from 'koa-bodyparser'
 import { setupBookRoutes } from './src/books'
 import { setupWarehouseRoutes } from './src/warehouse'
 
@@ -13,12 +18,31 @@ qs(app)
 // And we add cors to ensure we can access our API from the mcmasterful-books website
 app.use(cors())
 
+// Needed fot tsoa routes
+app.use(bodyParser())
+
 const router = zodRouter({ zodRouter: { exposeRequestErrors: true } })
 
 setupBookRoutes(router)
 setupWarehouseRoutes(router)
 
 app.use(router.routes())
+
+// tsoa routes
+const tsoaRouter = new Router()
+RegisterRoutes(tsoaRouter)
+app.use(tsoaRouter.routes())
+app.use(tsoaRouter.allowedMethods())
+
+// swagger ui
+app.use(
+  koaSwagger({
+    routePrefix: '/docs',
+    specPrefix: '/docs/spec',
+    exposeSpec: true,
+    swaggerOptions: { spec: swagger }
+  })
+)
 
 app.listen(3000, () => {
   console.log('listening!')
