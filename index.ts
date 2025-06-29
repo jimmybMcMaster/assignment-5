2425a411eb054ec1d30cbd73eb4c5e1d83e0f3ec
James Barbour <jameskbarbour1@gmail.com>
2025-06-29 16:28:18 +0000

    Clear ts error

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import { createServer } from './server'
 
 createServer(3000)
-console.log('listening on port 3000')
+  .then(() => {
+    console.log('Listening on port 3000')
+  })
+  .catch((error) => {
+    console.error('Error starting server:', error)
+  })
